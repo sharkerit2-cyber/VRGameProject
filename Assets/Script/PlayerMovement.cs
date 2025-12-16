325fb89c34a775053156df5986f263eac4030399
Anchu <chuan14402@github.com>
2025-12-17 03:38:12 +0800

    add gitignore

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 10f;
+    public float mouseSensitivity = 2f;
+
+    float yRotation = 0f;
+    CharacterController controller;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * h + transform.forward * v;
+        controller.Move(move * speed * Time.deltaTime);
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * 100f * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * 100f * Time.deltaTime;
+
+        yRotation -= mouseY;
+        yRotation = Mathf.Clamp(yRotation, -80f, 80f);
+
+        Camera.main.transform.localRotation = Quaternion.Euler(yRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
